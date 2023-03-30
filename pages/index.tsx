ac80dce813e9c7b17f2ce3e697cb7d21c2ed4bf0
sebasbocanegrab <sbocanegra17@misena.edu.co>
2023-03-30 18:33:23 -0500

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,7 +89,7 @@ export default function Home() {
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                 </span>
-                Ingresar
+                Ingresar a la plataforma
               </button>
               </Link>
             </div>
